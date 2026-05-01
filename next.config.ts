ef772c5bd2aeddb30108f8c89a7247e4975de07c
= <=>
2026-05-01 22:51:57 +0200

    css-user-class-fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'sample-videos.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.soundjay.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
